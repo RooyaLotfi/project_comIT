76aaea7042f4a7bae77a4a5c042852a5e398e494
Roya Lotfi <lotfir@myumanitoba.ca>
2020-11-01 15:05:23 -0600

    added class timeframe and some basic properties

---
+++ Models/TimeFrame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEvent.Dal.Models
+{
+    class TimeFrame
+    {
+        public int Id { get; set; }
+
+        public DateTime StartFrame { get; set; }
+
+        public DateTime EndFrame { get; set; }
+    }
+}
